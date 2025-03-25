6f3a9d6ee4d08fe79271e6070d8b38623cdfc030
Abhi-Na20 <nalatwadabhinav@gmail.com>
2025-03-25 15:12:32 +0000

    Initial file upload

---
+++ project.js
@@ -0,0 +1,19 @@
+function toggleList() {
+    // Access the List
+  const courses = document.getElementById("courses_container");
+  
+    // Check the visibility
+  if (courses.style.display == "block") {
+      courses.style.display = "none"; // Hide the list
+  } else {
+      courses.style.display = "block"; // Display the list
+  }
+}
+
+// Hide the list on click outside the dropdown list
+window.onclick = function (event) {
+  if (!event.target.matches('.dropdown_button')) {
+      document.getElementById('courses_container')
+          .style.display = "none";
+  }
+} 
