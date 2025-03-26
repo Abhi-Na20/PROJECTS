4acb6c94d6efd2c62e3b1e1da657b9fc0e03c0f5
NathanMulvenna <nmulvenna123@gmail.com>
2025-03-26 15:23:02 +0000

    Add files via upload

---
+++ abhi/project.js
@@ -0,0 +1,29 @@
+document.getElementById('contactBtn').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default anchor behavior
+    alert('Contact us at: ulstermotors@gmail.com');
+});
+
+
+document.getElementById('learnMoreBtn').addEventListener('click', function() {
+    alert('Thank you for your interest! More information will be available soon.');
+});
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
