939510263728c1721c503d13856ced0dfe65fdc6
Abhi-Na20 <nalatwadabhinav@gmail.com>
2025-05-22 11:51:46 +0400

    Delete nathan directory

--- nathan/Website/project.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('contactBtn').addEventListener('click', function(event) {
-    event.preventDefault(); // Prevent the default anchor behavior
-    alert('Contact us at: ulstermotors@gmail.com');
-});
-
-
-document.getElementById('learnMoreBtn').addEventListener('click', function() {
-    alert('Thank you for your interest! More information will be available soon.');
-});
